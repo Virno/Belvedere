9889208ea2c815aa5f707e51991402dba5565f21
Virno <44190545+Virno@users.noreply.github.com>
2020-12-16 14:56:51 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("Nav").style.width ="60%";
+}
+function openNav2() {
+    document.getElementById("Nav2").style.width ="60%";
+}
+function openNav3() {
+    document.getElementById("Nav3").style.width ="-60%";
+}
+function openNav4() {
+    document.getElementById("Nav4").style.width ="-60%";
+}
