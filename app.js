d1cde3da2f741e13c9d7716663ea7de182ab4875
Virno <dachefloran@gmail.com>
2020-12-17 13:07:39 +0100

    1.0

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ function openNav2() {
     document.getElementById("Nav2").style.width ="60%";
 }
 function openNav3() {
-    document.getElementById("Nav3").style.width ="60%";
+    document.getElementById("Nav3").style.width ="-60%";
 }
 function openNav4() {
-    document.getElementById("Nav4").style.width = "60%";
+    document.getElementById("Nav4").style.width = "-60%";
 }
